4c1bc2e8adb8dd50ff49e36733126542697f9c40
Ward Bekker <ward@tty.nl>
2012-09-03 13:47:47 +0200

    Using pattern matching to get failures

--- test/basic_SUITE.erl
+++ test/basic_SUITE.erl
@@ -1,13 +1,13 @@
 -module(basic_SUITE).
 -include_lib("common_test/include/ct.hrl").
+
 -export([all/0]).
 -export([test1/1, test2/1]).
 
 all() -> [test1,test2].
 
 test1(_Config) ->
-1 = 1.
+    3 = ci_quickstart_math:addition(1,2).
 
 test2(_Config) ->
-A = 0,
-1/A.
+    3 = ci_quickstart_math:addition(1,1).
